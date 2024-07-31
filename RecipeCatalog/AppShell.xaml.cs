849c3b813f70e180e846f8b410aaeda1b904c0be
Robin Kirch <robin.kirch@bechtle.com>
2024-07-31 12:37:03 +0200

    Add project files.

---
+++ RecipeCatalog/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeCatalog
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RecipeCatalog/Models/Components.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeCatalog.Models
+{
+    internal class Components
+    {
+        public byte[]? Image {  get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string[]? Aliases { get; set; }
+    }
+}
